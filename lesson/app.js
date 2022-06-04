b024c89612c2a822c1cb4d4015380300033dc481
Your Name <beksultanovich,911@gmail.com>
2022-06-04 19:33:24 +0600

    lesson

---
+++ lesson/app.js
@@ -0,0 +1,56 @@
+// const a = "a";//string
+//
+// let c = 32;//nuber
+//
+// const bool = true; //boolean
+// const bool2 = false;
+//
+// console.log(typeof bool)
+//
+//  const obj = {
+//      key: "value",
+//      username: "aidana",
+//      data :{
+//          first_name:"jack",
+//         last_name:"rus"
+//     }
+//
+// }
+//console.log(obj.data.last_name)
+//const arr = ["da",32,true,false]
+
+// console.log(typeof arr)
+// console.log(arr)
+
+// const name = prompt (  "как тебя зовут")
+// console.log("hello"+name)
+//
+// const first_nuber = prompt ( "первоу число")
+// const last_number = prompt("второе число")
+//
+// if (first_nuber < last_number){
+//     console.log(first_nuber + "<" + last_number)
+// }
+//     else if (first_nuber > last_number)
+//     {
+//     console.log(first_nuber + ">" + last_number)
+//     }
+//
+// else{
+//     console.log(first_nuber + "=" + last_number)
+// }
+
+// const  age = prompt("сколько вам лет")
+//     if (age < 18){
+//         console.log("вам нельзя входить")
+//     }
+//     else if(age > 18){
+//         console.log("можно")
+//     }else {
+//     console.log("спорно")
+// }
+
+
+
+const arr2 = [[[[[[[[[[[[[[[5]]]]]]]]]]]]]]]
+console.log(arr2[0.15])
